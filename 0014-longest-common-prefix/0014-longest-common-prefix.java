749725aef4b3ca05c75f4bc37c2027e214d6e7be
Karthick Raja <126078435+karthickraja06@users.noreply.github.com>
2025-01-17 20:19:42 +0530

    Time: 2 ms (22.94%), Space: 41.7 MB (41.47%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,17 +1,36 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        StringBuilder longestPref = new StringBuilder(strs[0]);
-        for(int i = 1;i < strs.length;i++){
-            int j = 0, m = longestPref.length();
-            for(;j < strs[i].length();j++){
-                if(j < m && longestPref.charAt(j) != strs[i].charAt(j)){
-                    break;
-                }else if(j >= m) break;
-            }
-            longestPref.delete(j,m);
-            if(m == 0)return "";
-            System.out.println(longestPref);
+    public boolean isPrefix(String[] arr,int s,int e,int n){
+        String pref = (arr[0].length() - 1 >= e)?arr[0].substring(s, e + 1):"";
+        if(pref == "")return false;
+        for(int i = 1;i < n;i++){
+            if(arr[i].length() - 1 < e)return false;
+        // System.out.println(pref + " " + arr[i].substring(s, e + 1));
+            if(!pref.equals(arr[i].substring(s, e + 1)))return false;
         }
-        return longestPref.toString();
+        return true;
+    }
+    public String longestCommonPrefix(String[] strs) {
+        int n = strs.length,minLen = Integer.MAX_VALUE; 
+        if(n == 1)return strs[0];
+        //to find the index of the minimum length word
+        for(int i = 0;i < n;i++){
+            int m = strs[i].length();
+            if(m == 0)return ""; //if any word is empty there cannot be any longer options left
+            if(minLen > m){
+                minLen = m;
+            }
+        }
+        
+        int l = 0,h = minLen - 1;
+        while(l <= h){
+            int mid = (l + h)/2;
+                System.out.println(h);
+            if(isPrefix(strs,l,mid,n)){
+                l = mid + 1;
+            }else{
+                h = mid - 1;
+            }
+        }
+        return strs[0].substring(0, l);
     }
 }
