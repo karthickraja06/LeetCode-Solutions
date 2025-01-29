397f63944e2c1202d5a38752db5b87fcf57985bc
Karthick Raja <126078435+karthickraja06@users.noreply.github.com>
2025-01-29 16:48:41 +0530

    Time: 104 ms (45.07%), Space: 64.6 MB (5.07%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,18 +1,19 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n == 0 || n == 1)return 0;
-        int[] prime = new int[n + 1];//+1 is to considering zero too
-
-        for(int i = 2; i < Math.sqrt(n);i++){
-            if(prime[i] == 0){
-                for(int j = i;i*j < n;j++){
-                    prime[i * j] = 1;
+        if (n < 2) return 0;
+        
+        int[] prime = new int[n];  // 0 means prime
+        for (int i = 2; i * i < n; i++) {
+            if (prime[i] == 0) {
+                for (int j = i * i; j < n; j += i) { // Start from i*i
+                    prime[j] = 1;
                 }
             }
         }
+        
         int res = 0;
-        for(int i = 2;i < n;i++){
-            if(prime[i] == 0)res++;
+        for (int i = 2; i < n; i++) {
+            if (prime[i] == 0) res++;
         }
         return res;
     }
